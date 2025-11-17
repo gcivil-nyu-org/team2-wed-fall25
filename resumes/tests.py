3aae3f15e29c04cee7977353f03c5fad97a3b8e2
Manish Kumbham <mk9654@nyu.edu>
2025-11-17 19:44:26 +0530

    Update tests.py

--- resumes/tests.py
+++ resumes/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Resume
+
+class ResumeViewTests(TestCase):
+    def setUp(self):
+        self.resume = Resume.objects.create(title="Test Resume")
+
+    def test_resume_list_view(self):
+        url = reverse("resumes:list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Resume")
+
+    def test_resume_detail_view(self):
+        url = reverse("resumes:detail", args=[self.resume.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Resume")
 
 # Create your tests here.
