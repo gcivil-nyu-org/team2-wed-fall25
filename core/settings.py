aed01a6fb709bf7419296a9c70866e876da894ee
Manish Kumbham <mk9654@nyu.edu>
2025-12-10 18:57:40 -0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -21,14 +21,14 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 ALLOWED_HOSTS = [
     "*",
     "marshall-cooling-clause-buffalo.trycloudflare.com",
-    "https://total-mobility-ability-schemes.trycloudflare.com",
+    "total-mobility-ability-schemes.trycloudflare.com",
     "likewise-recovered-port-golden.trycloudflare.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://marshall-cooling-clause-buffalo.trycloudflare.com",
     "https://total-mobility-ability-schemes.trycloudflare.com",
-    "likewise-recovered-port-golden.trycloudflare.com",
+    "https://likewise-recovered-port-golden.trycloudflare.com",
 ]
 
 # ----------------------------
