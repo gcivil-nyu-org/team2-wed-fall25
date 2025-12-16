21f47b45d391ec838bac863bc558db65607b760f
Manish Kumbham <mk9654@nyu.edu>
2025-12-16 10:53:09 -0500

    Update views.py

--- forums/views.py
+++ forums/views.py
@@ -4,10 +4,8 @@ from django.core.paginator import Paginator
 from django.db.models import Count, Max, Q
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.utils import timezone
-
 from .forms import CommentForm, PostForm, TopicForm
-from .models import Comment, Forum, MentorAssignment, Post, PostVote, Topic
+from .models import Forum, MentorAssignment, Post, PostVote, Topic
 
 
 def forum_list(request):
@@ -99,13 +97,7 @@ def topic_create(request, forum_id):
             topic.author = request.user
             topic.save()
 
-            # Create initial post
-            initial_post = Post.objects.create(
-                topic=topic,
-                author=request.user,
-                content=form.cleaned_data["description"],
-            )
-
+            # Create initial post, unused assignment so removed for flake8
             messages.success(request, "Topic created successfully!")
             return redirect("topic_detail", topic_id=topic.pk)
     else:
